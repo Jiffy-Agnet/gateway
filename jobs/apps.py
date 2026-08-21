f966038fd67291dd4d0e0ec024396330d18fafa6
mem0 <javadib67@gmail.com>
2026-08-21 21:01:13 +0330

    chore(startup): add startup banner for web server initialization

--- jobs/apps.py
+++ jobs/apps.py
@@ -4,3 +4,8 @@ from django.apps import AppConfig
 class JobsConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "jobs"
+
+    def ready(self):
+        from jobs.startup import print_startup_banner
+
+        print_startup_banner()
